2704ffdb22dd37b4fa74b5c630f09f769766a5cf
maycotterojas <maycotterojas1@gmail.com>
2025-11-27 05:40:59 -0500

    Update mr.js

--- mr.js
+++ mr.js
@@ -1,1 +1,19 @@
+function openCategory(evt, categoryName){
+  var i, tabcontent, tablinks;
+  
+  tabcontent = 
+document.getElementsByClassName("tabcontent");
+  for(i=0; i<tabcontent.length; i++){
+    tabcontent[i].style.display = "none";
+  }
 
+  tablinks =
+document.getElementsByClassName("tablinks");
+  for(i=0; i<tablinks.length; i++){
+    tablinks[i].className = tablinks[i].className.replace(" active","");
+  }
+
+  document.getElementByID(categoryName).style.display
+= "block";
+  evt.currentTarget.className += "active";
+}
