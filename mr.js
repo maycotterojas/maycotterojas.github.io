26c15536479577bd50d70a3e08bc03a0844f7af5
maycotterojas <maycotterojas1@gmail.com>
2025-11-27 06:08:27 -0500

    Update mr.js

--- mr.js
+++ mr.js
@@ -1,24 +1,24 @@
-<script>
-function openCategory(evt, categoryName){
-  var i, tabcontent, tablinks;
+//<script>
+//function openCategory(evt, categoryName){
+//  var i, tabcontent, tablinks;
   
-  tabcontent = 
-document.getElementsByClassName("tabcontent");
-  for(i=0; i<tabcontent.length; i++){
-    tabcontent[i].style.display = "none";
-  }
+//  tabcontent = 
+//document.getElementsByClassName("tabcontent");
+//  for(i=0; i<tabcontent.length; i++){
+//    tabcontent[i].style.display = "none";
+//  }
 
-  tablinks =
-document.getElementsByClassName("tablinks");
-  for(i=0; i<tablinks.length; i++){
-    tablinks[i].className = 
-tablinks[i].className.replace(" active", "");
-  }
+//  tablinks =
+//document.getElementsByClassName("tablinks");
+//  for(i=0; i<tablinks.length; i++){
+//    tablinks[i].className = 
+//tablinks[i].className.replace(" active", "");
+//  }
 
-  document.getElementById(categoryName).style.display
-= "block";
-  evt.currentTarget.className += " active";
-}
+//  document.getElementById(categoryName).style.display
+= //"block";
+//  evt.currentTarget.className += " active";
+//}
 
 //Show a tab by default
 //<button class="tablinks" onlinks="openCategory(event,'Drafting')" id="defaultOpen">Drafting</button>
